62298968eb1d380b74bf260350b2d3d6eaf1c833
ShapeoftheClouds <amandagracebutt@gmail.com>
2017-07-24 22:59:24 -0600

    added basic files

---
+++ BasicCard.js
@@ -0,0 +1,14 @@
+
+// Node module exporting a constructor.
+module.exports = BasicCard;
+
+// The constructor should accept two arguments: front and back.
+// Constructor accepting two arguments, front and back.
+var basicFlashCard = function (front, back) {
+	console.log("Card stuff goes here!");
+	// constructor should have a property for the front of the card containing the text.
+	// constructor should have a property for the back of the card containing the text.
+}; 
+
+// The constructed object should have a front property that contains the text on the front of the card.
+// The constructed object should have a back property that contains the text on the back of the card.
---
+++ ClozeCard.js
@@ -0,0 +1,13 @@
+// A module that exports a constructor from creating cloze-deletion flashcards.
+// This file should define a Node module that exports a constructor for creating cloze-deletion flashcards, e.g.:
+// module.exports = ClozeCard;
+module.exports = ClozeCard;
+// The constructor should accept two arguments: text and cloze.
+var cloze = function(text, cloze) {
+	// Close stuff goes here.
+}
+// The constructed object should have a cloze property that contains only the cloze-deleted portion of the text.
+// The constructed object should have a partial property that contains only the partial text.
+// The constructed object should have a fullText property that contains only the full text.
+// The constructor should throw or log an error when the cloze deletion does not appear in the input text.
+// Use prototypes to attach these methods, wherever possible.
