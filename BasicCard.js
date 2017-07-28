5fc211807017239a5a89084a3b2b98bfd6578828
ShapeoftheClouds <amandagracebutt@gmail.com>
2017-07-27 19:45:20 -0600

    working cloze code

--- BasicCard.js
+++ BasicCard.js
@@ -1,14 +1,85 @@
 
 // Node module exporting a constructor.
-module.exports = BasicCard;
+module.exports = flashCard;
+var inquirer = require("inquirer");
+var fs = require("fs");
+var userInput = process.argv[2];
+var newCardArray = [];
+
+inquirer.prompt ({
+		name: "flashcard",
+		type: "list",
+		message: "Would you like to [CREATE] flashcards or [REVIEW] flashcards?",
+		choices: ["CREATE", "REVIEW"]
+	})
+.then(function(response){
+	if (response.flashcard.toUpperCase() === "CREATE") {
+		console.log("You've created a flashcard!");
+		createFlashCards();
+	} else {
+		console.log("You're reviewing flashcards!");
+		reviewFlashCards();
+	}
+})
+
 
 // The constructor should accept two arguments: front and back.
 // Constructor accepting two arguments, front and back.
-var basicFlashCard = function (front, back) {
-	console.log("Card stuff goes here!");
+function flashCard(front, back){
+	this.front = front; 
+	this.back = back;
+
+};
+
+var firstPresident = new flashCard("Who was the first president of the United States?", "George Washington");
+
+	// console.log(firstPresident.back);
+
+// Function to create new flashcards.
+function createFlashCards() {
+	inquirer.prompt ([
+	{
+		name: "frontcard",
+		type: "input",
+		message: "Type in flashcard question"
+	}, 
+	{
+		name: "backcard",
+		type: "input",
+		message: "Type in flashcard answer"
+	}
+	])
+	.then(function(newCard){
+		console.log("Creating a new card!")
+		newCardArray.push(new flashCard(newCard.frontcard, newCard.backcard));
+		fs.appendFile("./cards.js" + newCardArray, function(err) {
+			if(err) {
+				return console.log(err);
+			}
+		});
+		console.log(newCardArray);
+		reviewFlashCards();
+	});
+};
+
+// Function to review already created flashcards.
+function reviewFlashCards() {
+	inquirer.prompt ({
+		name: "president", 
+		type: "input",
+		message: firstPresident.front,
+	}).then(function(response){
+		if (response.president === "George Washington") {
+			console.log("Correct! " + firstPresident.back + " was the first President of the U. S. A.");
+		} else {
+			console.log("Try again! " + firstPresident.back + " was the first President of the U. S. A.");
+		}
+	})
+};
+
 	// constructor should have a property for the front of the card containing the text.
 	// constructor should have a property for the back of the card containing the text.
-}; 
+
 
 // The constructed object should have a front property that contains the text on the front of the card.
 // The constructed object should have a back property that contains the text on the back of the card.
--- ClozeCard.js
+++ ClozeCard.js
@@ -1,13 +1,31 @@
-// A module that exports a constructor from creating cloze-deletion flashcards.
-// This file should define a Node module that exports a constructor for creating cloze-deletion flashcards, e.g.:
-// module.exports = ClozeCard;
-module.exports = ClozeCard;
+var fs = require("fs");
+var inquirer = require("inquirer");
+
 // The constructor should accept two arguments: text and cloze.
-var cloze = function(text, cloze) {
+var ClozeCard = function(text, cloze) {
 	// Close stuff goes here.
+	this.text = text;
+	this.cloze = cloze;
 }
+
+var firstPresidentCloze = new ClozeCard(
+    "George Washington was the first president of the United States.", "George Washington");
+
+// "George Washington"
+console.log(firstPresidentCloze.cloze); 
+
+// " ... was the first president of the United States.\
+var partial = firstPresidentCloze.text.split("George Washington ").pop();
+console.log(partial);
+
+// "George Washington was the first president of the United States.
+console.log(firstPresidentCloze.text);
+
+// Should throw or log an error because "oops" doesn't appear in "This doesn't work"
+var brokenCloze = new ClozeCard("This doesn't work", "oops");
 // The constructed object should have a cloze property that contains only the cloze-deleted portion of the text.
 // The constructed object should have a partial property that contains only the partial text.
 // The constructed object should have a fullText property that contains only the full text.
 // The constructor should throw or log an error when the cloze deletion does not appear in the input text.
 // Use prototypes to attach these methods, wherever possible.
+module.exports = ClozeCard;
