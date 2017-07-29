01f6116ed6adb9619b9d46bc294ab392b83fdb91
ShapeoftheClouds <amandagracebutt@gmail.com>
2017-07-29 10:14:23 -0600

    refining code

--- BasicCard.js
+++ BasicCard.js
@@ -1,11 +1,13 @@
 
 // Node module exporting a constructor.
 module.exports = flashCard;
+// Require the inquirer npm
 var inquirer = require("inquirer");
+// Require the fs npm
 var fs = require("fs");
-var userInput = process.argv[2];
+// Create an array that new flash cards are pushed into
 var newCardArray = [];
-
+// prompts the user to create or review flashcards
 inquirer.prompt ({
 		name: "flashcard",
 		type: "list",
@@ -13,30 +15,30 @@ inquirer.prompt ({
 		choices: ["CREATE", "REVIEW"]
 	})
 .then(function(response){
+	// If the response is create, the run the createFlashCard function
 	if (response.flashcard.toUpperCase() === "CREATE") {
 		console.log("You've created a flashcard!");
 		createFlashCards();
 	} else {
+		// If the response is not create, run the reviewFlashCard function
 		console.log("You're reviewing flashcards!");
 		reviewFlashCards();
 	}
 })
 
-
-// The constructor should accept two arguments: front and back.
-// Constructor accepting two arguments, front and back.
+// A Constructor accepting two arguments, front and back.
 function flashCard(front, back){
 	this.front = front; 
 	this.back = back;
-
 };
 
-var firstPresident = new flashCard("Who was the first president of the United States?", "George Washington");
-
-	// console.log(firstPresident.back);
+// Creating a basic flashcard for testing purposes
+var firstPresident = new flashCard(
+	"Who was the first president of the United States?", "George Washington");
 
 // Function to create new flashcards.
 function createFlashCards() {
+	// Prompts the user to write a question an answer.
 	inquirer.prompt ([
 	{
 		name: "frontcard",
@@ -50,15 +52,15 @@ function createFlashCards() {
 	}
 	])
 	.then(function(newCard){
-		console.log("Creating a new card!")
+		console.log("Creating a new card!");
+		// Pushes the users input into a new card.
 		newCardArray.push(new flashCard(newCard.frontcard, newCard.backcard));
-		fs.appendFile("./cards.js" + newCardArray, function(err) {
+		// Appends the new flash card to the text file.
+		fs.appendFile("./cards.txt" + newCardArray, function(err) {
 			if(err) {
 				return console.log(err);
 			}
 		});
-		console.log(newCardArray);
-		reviewFlashCards();
 	});
 };
 
--- ClozeCard.js
+++ ClozeCard.js
@@ -1,11 +1,50 @@
 var fs = require("fs");
 var inquirer = require("inquirer");
+var newCardArray = [];
 
 // The constructor should accept two arguments: text and cloze.
 var ClozeCard = function(text, cloze) {
 	// Close stuff goes here.
 	this.text = text;
 	this.cloze = cloze;
+}
+
+inquirer.prompt ({
+	name: "flashcard",
+	type: "list", 
+	message: "Would you like to [CREATE] flashcards or [REVIEW] flashcards?",
+	choices: ["CREATE", "REVIEW"]
+})
+.then(function(response){
+	if (response.flashcard.toUpperCase() === "CREATE") {
+		console.log("Create a flashcard!");
+		createFlashCards();
+	}
+});
+
+function createFlashCards() {
+	inquirer.prompt ([
+	{
+		name: "frontcard",
+		type: "input",
+		message: "Type in flashcard question"
+	}, 
+	{
+		name: "backcard",
+		type: "input",
+		message: "Type in flashcard answer"
+	}
+	])
+	.then(function(response){
+		console.log("Creating a new card!")
+		newCardArray.push(new ClozeCard(response.frontcard, response.backcard));
+		fs.appendFile("./cards.txt", ", " + newCardArray, function(err) {
+			if(err) {
+				return console.log(err);
+			}
+		});
+		console.log(newCardArray);
+	});
 }
 
 var firstPresidentCloze = new ClozeCard(
